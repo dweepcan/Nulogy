1234fc17736e0eb743378c1ee2d362e4139ae27e
Dweep Shah <dweepcan1@msn.com>
2014-10-09 18:22:09 -0400

    Parsing completed

--- src/NuPack.java
+++ src/NuPack.java
@@ -1,13 +1,34 @@
 import java.util.Scanner;
 
+/**
+ * Main class of the program
+ *  
+ * @author Dweep
+ */
 public class NuPack {
 	public static void main(String[] args) {
+		// Initialize standard input
 		Scanner in = new Scanner(System.in);
-
-		double basePrice = Parser.parseBasePrice(in.nextLine().trim()); 
+		
+		// Ignore any blank lines entered
+		String input = Parser.removeBlankLines(in, in.nextLine().trim());
+		
+		// Parse the given basePrice to get the double value
+		double basePrice = Parser.parseBasePrice(input); 
 		System.out.println(basePrice);
 		
-		int workers = Parser.parseWorkers(in.nextLine().trim());
-		System.out.println(workers);	
+		// Ignore any blank lines entered
+		input = Parser.removeBlankLines(in, in.nextLine().trim());
+		
+		// Parse the given workers value to get the integer value
+		int workers = Parser.parseWorkers(input);
+		System.out.println(workers);
+		
+		// Ignore any blank lines entered
+		input = Parser.removeBlankLines(in, in.nextLine().trim());
+		
+		// Store the value entered for material
+		String material = in.nextLine().trim();
+		System.out.println(material);
 	}
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Class used to parse the input given and check for any invalid input values.
+ * 
+ * @author Dweep
+ */
 public class Parser {
-	static double parseBasePrice(String basePriceString) {
+	/**
+	 * The base price value entered in standard input is parsed.
+	 * Error checking is done on the value entered, and if no errors 
+	 * exist the corresponding double value is returned.
+	 * 
+	 * @param basePriceString Base Price given on standard input
+	 * @return parsed base price value after converting to double
+	 */
+	public static double parseBasePrice(String basePriceString) {
+		// Check if the base price entered starts with a $ sign
 		if(!basePriceString.startsWith("$")) {
 			System.err.println("The base price entered does not begin with a $ sign.");
 			System.exit(1);
@@ -9,6 +25,7 @@ public class Parser {
 
 		int basePriceDecimal = basePriceNum.indexOf('.');
 
+		// Check if the base price entered is a dollar amount with 2 decimals.
 		if(basePriceDecimal == -1 || basePriceNum.substring(basePriceDecimal+1).length() != 2) {
 			System.err.println("The base price entered is not a dollar amount with 2 decimal places.");
 			System.exit(1);
@@ -16,6 +33,7 @@ public class Parser {
 		
 		double basePrice = 0.0;
 
+		// Check if the base price entered is a decimal value
 		try {
 			basePrice = Double.parseDouble(basePriceNum);
 		} catch(Exception e) {
@@ -24,17 +42,28 @@ public class Parser {
 			e.printStackTrace();
 		}
 		
+		// Check if the base price entered is negative
 		if(basePrice < 0) {
 			System.err.println("The base price entered is negative.");
 			System.exit(1);
-		} else if (basePrice == 0) {
+		} 
+		// Check if the base price entered is zero
+		else if (basePrice == 0) {
 			System.err.println("The base price entered is zero");
 		}
 		
 		return basePrice;
 	}
 	
-	static int parseWorkers(String workersString) {
+	/**
+	 * The number of workers value entered in standard input is parsed.
+	 * Error checking is done on the value entered, and if no errors 
+	 * exist the corresponding integer value is returned.
+	 * 
+	 * @param workersString number of workers given on standard input
+	 * @return parsed worker value after converting to integer
+	 */
+	public static int parseWorkers(String workersString) {
 		int workersStringSpace = workersString.indexOf(" ");
 
 		if(workersStringSpace == -1) {
@@ -62,7 +91,7 @@ public class Parser {
 		
 		String workersAfterNum = workersString.substring(workersString.indexOf(" ")).trim().toLowerCase();
 		
-		if(workersAfterNum.equals("people") && workersAfterNum.equals("person")) {
+		if(!workersAfterNum.equals("people") && !workersAfterNum.equals("person")) {
 			System.err.println("The number of workers entered does not have people or person after the number.");
 			System.exit(1);
 		} else if(workers == 1 && workersAfterNum.equals("people")) {
@@ -75,5 +104,20 @@ public class Parser {
 		}
 		
 		return workers;
+	}
+	
+	/**
+	 * Skips all of the blank lines and returns the first non
+	 * empty line read from the given InputReader
+	 * 
+	 * @param in Input Reader used to read the input
+	 * @param input the last input line read from the input reader
+	 * @return the first non empty line read by the input reader
+	 */
+	public static String removeBlankLines(Scanner in, String input) {
+		while(input.length() == 0) {
+			input = in.nextLine().trim();
+		}
+		return input;
 	}
 }
